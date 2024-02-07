c7035c26868e4d70b3926bfbac44e91628d3abf2
KinYuan <wongky2228@gmail.com>
2024-02-07 16:38:48 -0500

    create

--- swerveRob/swerveAttempt/src/main/java/frc/robot/RobotContainer.java
+++ swerveRob/swerveAttempt/src/main/java/frc/robot/RobotContainer.java
@@ -5,24 +5,16 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.drivingCommand;
-import frc.robot.commands.zeroHeading;
-import frc.robot.subsystems.SwerveSubsystem;
+
 
 public class RobotContainer {
 
-    private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-    private final XboxController xc = new XboxController(0);
+
 
     //private final SendableChooser<Command> autoChooser;
 
     public RobotContainer() {
-        swerveSubsystem.setDefaultCommand(new drivingCommand(swerveSubsystem,
-        () -> xc.getLeftX(),
-        () -> xc.getLeftY(),
-        () -> xc.getRightX(),
-        () -> !xc.getYButton()));
-                
+      
 
         configureButtonBindings();
         //autoChooser = AutoBuilder.buildAutoChooser();
@@ -31,7 +23,7 @@ public class RobotContainer {
     }
 
     private void configureButtonBindings() {
-        new JoystickButton(xc, 2).onTrue(new zeroHeading(swerveSubsystem));
+        
     }
 
     public Command getAutonomousCommand() {
